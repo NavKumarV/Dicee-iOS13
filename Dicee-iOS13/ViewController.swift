8da01ebb3e475f6e2f4c384b45570fc115b32c20
NavKumarV <naveenkumar.vamd@gmail.com>
2020-06-22 11:11:53 +0530

    randomisation of the dice output

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        // Generic way. auto random range
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+        // specifying the random range
+//        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+//        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
     }
-
-
+    
 }
 
